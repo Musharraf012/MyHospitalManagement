6ed8abcf8fc32a126cd74df2896fda12e5fe83ca
Musharraf012 <157002498+Musharraf012@users.noreply.github.com>
2025-05-10 20:37:47 +0530

    initial

---
+++ backend/app/controllers/user.controller.js
@@ -0,0 +1,130 @@
+const db = require("../models");
+const User = db.user;
+
+// Create and Save a new User
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.name || !req.body.email || !req.body.password || !req.body.role ) {
+      res.status(400).send({ message: "Content can not be empty!" });
+      return;
+    }
+  
+    // Create a Tutorial
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      role:req.body.role
+    });
+  
+    // Save Tutorial in the database
+    user
+      .save()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the User."
+        });
+      });
+  };
+
+// Retrieve all User from the database.
+exports.findAll = (req, res) => {
+    const name = req.query.name;
+    var condition = name ? { name: { $regex: new RegExp(name), $options: "i" } } : {};
+  
+    User.find(condition)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving users."
+        });
+      });
+  };
+
+// Find a single User with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    User.findById(id)
+      .then(data => {
+        if (!data)
+          res.status(404).send({ message: "Not found User with id " + id });
+        else res.send(data);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({ message: "Error retrieving User with id=" + id });
+      });
+  };
+
+// Update a User by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update User with id=${id}. Maybe User was not found!`
+        });
+      } else res.send({ message: "User was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating User with id=" + id
+      });
+    });
+};
+
+// Delete a single user by ID
+exports.delete = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+      return res.status(404).send({
+        message: `Cannot delete User with id=${id}. Maybe User was not found!`,
+      });
+    }
+
+    res.send({ message: "User was deleted successfully!" });
+  } catch (err) {
+    res.status(500).send({
+      message: `Could not delete User with id=${id}`,
+      error: err.message,
+    });
+  }
+};
+
+
+//delete all users
+exports.deleteAll = (req, res) => {
+  User.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Users were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials."
+      });
+    });
+};
