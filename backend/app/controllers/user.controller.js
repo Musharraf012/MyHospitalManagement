353a3e6f2d813d220afc4816e56b73024fec1b19
Musharraf012 <157002498+Musharraf012@users.noreply.github.com>
2025-05-24 12:07:53 +0530

    feat: initialize frontend with Vite and React setup
    - Added package.json for project dependencies and scripts.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application.
    - Developed App.jsx as the main component with basic functionality.
    - Added React logo SVG in assets directory.
    - Established global styles in index.css.
    - Set up main entry point in main.jsx for rendering the application.
    - Configured Vite with vite.config.js for React support.

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -1,28 +1,11 @@
-exports.allAccess = (req, res) => {
-  res.status(200).send("Public Content.");
+const sendBoardContent = (content) => (req, res) => {
+  res.status(200).send(`${content} Content.`);
 };
 
-exports.superAdminBoard = (req, res) => {
-  res.status(200).send("Super Admin Content.");
-};
-
-exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
-};
-
-exports.doctorBoard = (req, res) => {
-  res.status(200).send("Doctor Content.");
-};
-
-exports.nurseBoard = (req, res) => {
-  res.status(200).send("Nurse Content.");
-};
-
-exports.receptionistBoard = (req, res) => {
-  res.status(200).send("Receptionist Content.");
-};
-
-exports.patientBoard = (req, res) => {
-  res.status(200).send("Patient Content.");
-};
-
+exports.allAccess = sendBoardContent("Public");
+exports.superAdminBoard = sendBoardContent("Super Admin");
+exports.adminBoard = sendBoardContent("Admin");
+exports.doctorBoard = sendBoardContent("Doctor");
+exports.nurseBoard = sendBoardContent("Nurse");
+exports.receptionistBoard = sendBoardContent("Receptionist");
+exports.patientBoard = sendBoardContent("Patient");
--- backend/app/models/role.model.js
+++ backend/app/models/role.model.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const Role = mongoose.model(
   "Role",
   new mongoose.Schema({
-    name: String
-  })
+    name: String,
+    enum: ["super admin", "admin", "doctor", "nurse", "receptionist", "patient"],
+
+  },
+    { timestamps: true })
 );
 
 module.exports = Role;
